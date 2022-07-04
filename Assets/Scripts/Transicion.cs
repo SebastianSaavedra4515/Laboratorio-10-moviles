71f943c7639da61e7f2e7e0718296f522ba4774a
sebastian <sebastiansaavedra4515@gmail.com>
2022-07-03 23:20:34 -0500

    s
    s

---
+++ Assets/Scripts/Transicion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Transicion : MonoBehaviour
+{
+    public float TiempoCambio = 1;
+    float tiempo = 0;
+    public string ScenaCambio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tiempo += Time.deltaTime;
+        if(tiempo>=TiempoCambio)
+        {
+            SceneManager.LoadScene(ScenaCambio);
+        }
+    }
+}
